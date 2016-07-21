6d07e1d2f1c3b83140fff801e7fb5fdf60ec8dba
muralisheri <murali.sheri@appsassociates.com>
2016-07-21 18:01:56 +0530

    Add files via upload

--- EC2.rb
+++ EC2.rb
@@ -10,18 +10,26 @@ template do
   value :AWSTemplateFormatVersion => '2010-09-09'
 
   value :Description => 'Ec2 block device mapping'
-
+ 
   resource 'MyEC2Instance', :Type => 'AWS::EC2::Instance', :Properties => {
-      :ImageId => 'ami-79fd7eee',
-      :KeyName => 'testkey',
-      :BlockDeviceMappings => [
+      :InstanceType => 't2.micro',
+      :ImageId => 'ami-00f07917',
+      :KeyName => 'chef-dk',
+	  :BlockDeviceMappings => [
           {
               :DeviceName => '/dev/sdm',
-              :Ebs => { :VolumeType => 'gpp2', :DeleteOnTermination => 'false', :VolumeSize => '20' },
+              :Ebs => { :VolumeType => 'gp2', :DeleteOnTermination => 'false', :VolumeSize => '20' },
           },
           {
               :DeviceName => '/dev/sdk',
               :NoDevice => {},
-          },
-      ],
+          }
+      ]
   }
+  resource 'myVPC', :Type => 'AWS::EC2::VPC', :Properties => {
+	  :CidrBlock => '10.0.0.0/24',
+      :EnableDnsSupport => 'true',
+      :EnableDnsHostnames => 'true',
+      :InstanceTenancy => 'default',
+  }
+end.exec!
