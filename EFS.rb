42fdfdab07868c295706f879aa12c83d7140fa17
muralisheri <murali.sheri@appsassociates.com>
2016-07-21 15:08:37 +0530

    Add files via upload

--- EFS.rb
+++ EFS.rb
@@ -11,13 +11,6 @@ template do
 
   value :Description => 'This template creates an Amazon EFS file system and mount target and associates it with Amazon EC2 instances in an Auto Scaling group. **WARNING** This template creates Amazon EC2 instances and related resources. You will be billed for the AWS resources used if you create a stack from this template.'
 
- parameter 'InstanceType',
-            :Description => 'NFS server EC2 instance type.',
-            :Type => 'String',
-            :Default => 'm1.medium',
-            :AllowedValues => [ 'm1.medium', 'm1.large', 'm1.xlarge', 'm2.xlarge', 'm2.2xlarge', 'm2.4xlarge', 'c1.medium', 'c1.xlarge' ],
-            :ConstraintDescription => 'Must be a valid EC2 instance type.'			
-			
  parameter 'SSHLocation',
             :Description => 'The IP address range that can be used to connect to the EC2 instances by using SSH',
             :Type => 'String',
@@ -138,7 +131,7 @@ template do
 
   resource 'LaunchConfiguration', :Type => 'AWS::AutoScaling::LaunchConfiguration', :Metadata => { :'AWS::CloudFormation::Init' => { :configSets => { :MountConfig => [ 'setup', 'mount' ] }, :setup => { :packages => { :yum => { :'nfs-utils' => [] } }, :files => { :'/home/ec2-user/post_nfsstat' => { :content => join('', "#!/bin/bash\n", "\n", "INPUT=\"$(cat)\"\n", "CW_JSON_OPEN='{ \"Namespace\": \"EFS\", \"MetricData\": [ '\n", "CW_JSON_CLOSE=' ] }'\n", "CW_JSON_METRIC=''\n", "METRIC_COUNTER=0\n", "\n", "for COL in 1 2 3 4 5 6; do\n", "\n", " COUNTER=0\n", " METRIC_FIELD=$COL\n", " DATA_FIELD=$(($COL+($COL-1)))\n", "\n", " while read line; do\n", "   if [[ COUNTER -gt 0 ]]; then\n", "\n", "     LINE=`echo $line | tr -s ' ' `\n", '     AWS_COMMAND="aws cloudwatch put-metric-data --region ', aws_region, "\"\n", "     MOD=$(( $COUNTER % 2))\n", "\n", "     if [ $MOD -eq 1 ]; then\n", "       METRIC_NAME=`echo $LINE | cut -d ' ' -f $METRIC_FIELD`\n", "     else\n", "       METRIC_VALUE=`echo $LINE | cut -d ' ' -f $DATA_FIELD`\n", "     fi\n", "\n", "     if [[ -n \"$METRIC_NAME\" && -n \"$METRIC_VALUE\" ]]; then\n", "       INSTANCE_ID=$(curl -s http://169.254.169.254/latest/meta-data/instance-id)\n", "       CW_JSON_METRIC=\"$CW_JSON_METRIC { \\\"MetricName\\\": \\\"$METRIC_NAME\\\", \\\"Dimensions\\\": [{\\\"Name\\\": \\\"InstanceId\\\", \\\"Value\\\": \\\"$INSTANCE_ID\\\"} ], \\\"Value\\\": $METRIC_VALUE },\"\n", "       unset METRIC_NAME\n", "       unset METRIC_VALUE\n", "\n", "       METRIC_COUNTER=$((METRIC_COUNTER+1))\n", "       if [ $METRIC_COUNTER -eq 20 ]; then\n", "         # 20 is max metric collection size, so we have to submit here\n", '         aws cloudwatch put-metric-data --region ', aws_region, " --cli-input-json \"`echo $CW_JSON_OPEN ${CW_JSON_METRIC%?} $CW_JSON_CLOSE`\"\n", "\n", "         # reset\n", "         METRIC_COUNTER=0\n", "         CW_JSON_METRIC=''\n", "       fi\n", "     fi  \n", "\n", "\n", "\n", "     COUNTER=$((COUNTER+1))\n", "   fi\n", "\n", "   if [[ \"$line\" == \"Client nfs v4:\" ]]; then\n", "     # the next line is the good stuff \n", "     COUNTER=$((COUNTER+1))\n", "   fi\n", " done <<< \"$INPUT\"\n", "done\n", "\n", "# submit whatever is left\n", 'aws cloudwatch put-metric-data --region ', aws_region, ' --cli-input-json "`echo $CW_JSON_OPEN ${CW_JSON_METRIC%?} $CW_JSON_CLOSE`"'), :mode => '000755', :owner => 'ec2-user', :group => 'ec2-user' }, :'/home/ec2-user/crontab' => { :content => join('', "* * * * * /usr/sbin/nfsstat | /home/ec2-user/post_nfsstat\n"), :owner => 'ec2-user', :group => 'ec2-user' } }, :commands => { :'01_createdir' => { :command => join('', 'mkdir /', ref('MountPoint')) } } }, :mount => { :commands => { :'01_mount' => { :command => join('', 'mount -t nfs4 $(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone).', ref('FileSystem'), '.efs.', aws_region, '.amazonaws.com:/ /', ref('MountPoint')) }, :'02_permissions' => { :command => join('', 'chown ec2-user:ec2-user /', ref('MountPoint')) } } } } }, :Properties => {
       :AssociatePublicIpAddress => true,
-      :InstanceType => ref('InstanceType'),
+      :InstanceType => 't2.micro',
       :KeyName => 'chef-dk',
       :SecurityGroups => [ ref('InstanceSecurityGroup') ],
       :IamInstanceProfile => ref('CloudWatchPutMetricsInstanceProfile'),
