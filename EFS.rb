b2813a903e8f2ff0856b3c79ffbabaaf80d66e6f
muralisheri <murali.sheri@appsassociates.com>
2016-07-21 14:53:11 +0530

    Add files via upload

--- EFS.rb
+++ EFS.rb
@@ -18,12 +18,7 @@ template do
             :AllowedValues => [ 'm1.medium', 'm1.large', 'm1.xlarge', 'm2.xlarge', 'm2.2xlarge', 'm2.4xlarge', 'c1.medium', 'c1.xlarge' ],
             :ConstraintDescription => 'Must be a valid EC2 instance type.'			
 			
-  parameter 'AsgMaxSize',
-            :Type => 'Number',
-            :Description => 'Maximum size and initial desired capacity of Auto Scaling Group',
-            :Default => '1'
-
-  parameter 'SSHLocation',
+ parameter 'SSHLocation',
             :Description => 'The IP address range that can be used to connect to the EC2 instances by using SSH',
             :Type => 'String',
             :MinLength => '9',
@@ -43,66 +38,6 @@ template do
             :Type => 'String',
             :MinLength => '1',
             :Default => 'myEFSvolume'
-
-  mapping 'AWSInstanceType2Arch',
-          :'t1.micro' => { :Arch => 'PV64' },
-          :'t2.micro' => { :Arch => 'HVM64' },
-          :'t2.small' => { :Arch => 'HVM64' },
-          :'t2.medium' => { :Arch => 'HVM64' },
-          :'m1.small' => { :Arch => 'PV64' },
-          :'m1.medium' => { :Arch => 'PV64' },
-          :'m1.large' => { :Arch => 'PV64' },
-          :'m1.xlarge' => { :Arch => 'PV64' },
-          :'m2.xlarge' => { :Arch => 'PV64' },
-          :'m2.2xlarge' => { :Arch => 'PV64' },
-          :'m2.4xlarge' => { :Arch => 'PV64' },
-          :'m3.medium' => { :Arch => 'HVM64' },
-          :'m3.large' => { :Arch => 'HVM64' },
-          :'m3.xlarge' => { :Arch => 'HVM64' },
-          :'m3.2xlarge' => { :Arch => 'HVM64' },
-          :'c1.medium' => { :Arch => 'PV64' },
-          :'c1.xlarge' => { :Arch => 'PV64' },
-          :'c3.large' => { :Arch => 'HVM64' },
-          :'c3.xlarge' => { :Arch => 'HVM64' },
-          :'c3.2xlarge' => { :Arch => 'HVM64' },
-          :'c3.4xlarge' => { :Arch => 'HVM64' },
-          :'c3.8xlarge' => { :Arch => 'HVM64' },
-          :'c4.large' => { :Arch => 'HVM64' },
-          :'c4.xlarge' => { :Arch => 'HVM64' },
-          :'c4.2xlarge' => { :Arch => 'HVM64' },
-          :'c4.4xlarge' => { :Arch => 'HVM64' },
-          :'c4.8xlarge' => { :Arch => 'HVM64' },
-          :'g2.2xlarge' => { :Arch => 'HVMG2' },
-          :'r3.large' => { :Arch => 'HVM64' },
-          :'r3.xlarge' => { :Arch => 'HVM64' },
-          :'r3.2xlarge' => { :Arch => 'HVM64' },
-          :'r3.4xlarge' => { :Arch => 'HVM64' },
-          :'r3.8xlarge' => { :Arch => 'HVM64' },
-          :'i2.xlarge' => { :Arch => 'HVM64' },
-          :'i2.2xlarge' => { :Arch => 'HVM64' },
-          :'i2.4xlarge' => { :Arch => 'HVM64' },
-          :'i2.8xlarge' => { :Arch => 'HVM64' },
-          :'d2.xlarge' => { :Arch => 'HVM64' },
-          :'d2.2xlarge' => { :Arch => 'HVM64' },
-          :'d2.4xlarge' => { :Arch => 'HVM64' },
-          :'d2.8xlarge' => { :Arch => 'HVM64' },
-          :'hi1.4xlarge' => { :Arch => 'HVM64' },
-          :'hs1.8xlarge' => { :Arch => 'HVM64' },
-          :'cr1.8xlarge' => { :Arch => 'HVM64' },
-          :'cc2.8xlarge' => { :Arch => 'HVM64' }
-
-  mapping 'AWSRegionArch2AMI',
-          :'us-east-1' => { :PV64 => 'ami-1ccae774', :HVM64 => 'ami-00f07917', :HVMG2 => 'ami-8c6b40e4' },
-          :'us-west-2' => { :PV64 => 'ami-ff527ecf', :HVM64 => 'ami-e7527ed7', :HVMG2 => 'ami-abbe919b' },
-          :'us-west-1' => { :PV64 => 'ami-d514f291', :HVM64 => 'ami-d114f295', :HVMG2 => 'ami-f31ffeb7' },
-          :'eu-west-1' => { :PV64 => 'ami-bf0897c8', :HVM64 => 'ami-a10897d6', :HVMG2 => 'ami-d5bc24a2' },
-          :'eu-central-1' => { :PV64 => 'ami-ac221fb1', :HVM64 => 'ami-a8221fb5', :HVMG2 => 'ami-7cd2ef61' },
-          :'ap-northeast-1' => { :PV64 => 'ami-27f90e27', :HVM64 => 'ami-cbf90ecb', :HVMG2 => 'ami-6318e863' },
-          :'ap-southeast-1' => { :PV64 => 'ami-acd9e8fe', :HVM64 => 'ami-68d8e93a', :HVMG2 => 'ami-3807376a' },
-          :'ap-southeast-2' => { :PV64 => 'ami-ff9cecc5', :HVM64 => 'ami-fd9cecc7', :HVMG2 => 'ami-89790ab3' },
-		  :'ap-south-1' => { :HVM64 => 'ami-ffbdd790' },
-          :'sa-east-1' => { :PV64 => 'ami-bb2890a6', :HVM64 => 'ami-b52890a8', :HVMG2 => 'NOT_SUPPORTED' },
-          :'cn-north-1' => { :PV64 => 'ami-fa39abc3', :HVM64 => 'ami-f239abcb', :HVMG2 => 'NOT_SUPPORTED' }
 
   resource 'CloudWatchPutMetricsRole', :Type => 'AWS::IAM::Role', :Properties => {
       :AssumeRolePolicyDocument => {
@@ -217,12 +152,12 @@ template do
       ),
   }
 
-  resource 'AutoScalingGroup', :Type => 'AWS::AutoScaling::AutoScalingGroup', :DependsOn => [ 'MountTarget' ], :CreationPolicy => { :ResourceSignal => { :Timeout => 'PT15M', :Count => ref('AsgMaxSize') } }, :Properties => {
+  resource 'AutoScalingGroup', :Type => 'AWS::AutoScaling::AutoScalingGroup', :DependsOn => [ 'MountTarget' ,'GatewayToInternet' ], :CreationPolicy => { :ResourceSignal => { :Timeout => 'PT15M', :Count => '1' } }, :Properties => {
       :VPCZoneIdentifier => [ ref('Subnet') ],
       :LaunchConfigurationName => ref('LaunchConfiguration'),
       :MinSize => '1',
-      :MaxSize => ref('AsgMaxSize'),
-      :DesiredCapacity => ref('AsgMaxSize'),
+      :MaxSize => '1',
+      :DesiredCapacity => '1',
       :Tags => [
           { :Key => 'Name', :Value => 'EFS FileSystem Mounted Instance', :PropagateAtLaunch => 'true' },
       ],
