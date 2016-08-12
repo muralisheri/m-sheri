b1c50e1cbb56df325aae1ee495e771c6ac6ada8d
muralisheri <murali.sheri@appsassociates.com>
2016-08-12 15:56:02 +0530

    Add files via upload

--- GithunEntp_EC2.rb
+++ GithunEntp_EC2.rb
@@ -54,7 +54,7 @@ template do
               :IpProtocol => 'tcp',
               :FromPort => '122',
               :ToPort => '122',
-              :CidrIp => ref('Subnet'),
+              :CidrIp => '0.0.0.0/0',
           },
           { :IpProtocol => 'tcp', :FromPort => '80', :ToPort => '80', :CidrIp => '0.0.0.0/0' },
 		],
