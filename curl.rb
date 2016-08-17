6b6a3468aee5590dec99ff67c43944ac9edc42a7
muralisheri <murali.sheri@appsassociates.com>
2016-08-17 16:01:20 +0530

    Add files via upload

--- curl.rb
+++ curl.rb
@@ -4,5 +4,5 @@ require 'uri'
 
 http_request 'posting data' do
   action :post
-  url 'curl -L '\https://52.91.41.6:8443/setup/api?api_key=Darling143\''
+  uri = URI.parse ('curl -L https://52.91.41.6:8443/setup/api?api_key=Darling143') 
 end
